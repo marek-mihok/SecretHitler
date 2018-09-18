7ac4547d8ad94ca37045076720f71bef686104cd
Michal Geci <michal.geci.1995@gmail.com>
2018-09-18 02:08:25 +0200

    Aminacie ale aj bordel :/

--- app/src/main/java/io/g3m/secrethitler/RevealIdentity.kt
+++ app/src/main/java/io/g3m/secrethitler/RevealIdentity.kt
@@ -1,5 +1,7 @@
 package io.g3m.secrethitler
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -22,11 +24,23 @@ class RevealIdentity : FullScreenActivity() {
         val density = resources.displayMetrics.density
 
         val fadeOutAnim = AnimationUtils.loadAnimation(this,R.anim.fade_out)
-        val animation = AnimationUtils.loadAnimation(this,R.anim.translate_x)
+        val fadeInAnim = AnimationUtils.loadAnimation(this,R.anim.fade_in)
+        val showBackAnim = AnimationUtils.loadAnimation(this,R.anim.show_back_in_middle)
+        val progressButtonAnimation = AnimationUtils.loadAnimation(this,R.anim.translate_x)
+
+        val shiftAnim = AnimationUtils.loadAnimation(this,R.anim.shift_back)
+
+        val envelopeFrontAnim = AnimatorInflater.loadAnimator(this,R.animator.rotate)
+        envelopeFrontAnim.setTarget(envelopeFront)
+
 
         progressView.alpha = 0F
         progressView.visibility = View.VISIBLE
-        animation.duration = 2000
+        progressButtonAnimation.duration = 1500
+
+        val distance = (160 * 18.1).toInt()
+        envelopeFront.cameraDistance = distance * density
+        envelopeBack.cameraDistance = distance * density
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             revealButtonTextView.letterSpacing = 0.05F
@@ -38,7 +52,7 @@ class RevealIdentity : FullScreenActivity() {
             override fun onAnimationEnd(animation: Animation?) {
                 progressView.alpha = 0F
                 if (shouldHideButton) {
-                    revealButton.visibility = View.GONE
+                    revealButton.visibility = View.INVISIBLE
                 }
             }
 
@@ -48,13 +62,15 @@ class RevealIdentity : FullScreenActivity() {
 
         })
 
-        animation.setAnimationListener(object: Animation.AnimationListener {
+        progressButtonAnimation.setAnimationListener(object: Animation.AnimationListener {
             override fun onAnimationRepeat(animation: Animation?) { }
 
             override fun onAnimationEnd(animation: Animation?) {
                 if (buttonClicked) {
                     shouldHideButton = true
                     this@RevealIdentity.vibrate()
+                } else {
+                    shouldHideButton = false
                 }
             }
 
@@ -64,27 +80,40 @@ class RevealIdentity : FullScreenActivity() {
         revealButton.setOnTouchListener { v, event ->
             v?.performClick()
 
-            when(event?.action){
-                MotionEvent.ACTION_DOWN -> {
-                    Log.d("debug","Pressed")
-                    revealButton.cardElevation = round(2 * density).toFloat()
-                    this@RevealIdentity.vibrate()
+            if (event.pointerCount < 2) {
+                when(event?.action){
+                    MotionEvent.ACTION_DOWN -> {
+                        Log.d("debug","Pressed")
+                        revealButton.cardElevation = round(2 * density).toFloat()
+                        this@RevealIdentity.vibrate()
 
-                    progressView.alpha = 1F
-                    buttonClicked = true
-                    progressView.startAnimation(animation)
+                        progressView.alpha = 1F
+                        buttonClicked = true
+                        progressView.startAnimation(progressButtonAnimation)
 
-                }
-                MotionEvent.ACTION_UP -> {
-                    Log.d("debug", "Released")
-                    revealButton.cardElevation = round(4 * density).toFloat()
+                        envelopeBack.startAnimation(showBackAnim)
+                        envelopeFront.startAnimation(shiftAnim)
+                        envelopeFrontAnim.start()
 
-                    buttonClicked = false
-                    progressView.clearAnimation()
-                    if (shouldHideButton) {
-                        revealButton.startAnimation(fadeOutAnim)
-                    } else {
-                        progressView.startAnimation(fadeOutAnim)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        Log.d("debug", "Released")
+                        revealButton.cardElevation = round(4 * density).toFloat()
+
+                        buttonClicked = false
+
+                        progressView.clearAnimation()
+                        if (shouldHideButton) {
+                            revealButton.startAnimation(fadeOutAnim)
+                        } else {
+                            progressView.startAnimation(fadeOutAnim)
+
+                            envelopeFrontAnim.start()
+                            envelopeFrontAnim.cancel()
+                            envelopeBack.clearAnimation()
+                            envelopeFront.clearAnimation()
+                            //envelopeFront.startAnimation(fadeInAnim)
+                        }
                     }
                 }
             }
